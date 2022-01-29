9345b4bced85f7b23fcbbff1fb751bc7615fecb8
McCoady <78250146+McCoady@users.noreply.github.com>
2022-01-29 11:06:58 +0900

    puppeteer added

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const path = require('path')
 const cool = require('cool-ascii-faces')
+const puppeteer = require('puppeteer');
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg');
+
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,7 +20,22 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/scrape', (req, res) => res.send(returnPage()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null };
+      res.render('pages/db', results);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
 
 showTimes = () => {
   let result = '';
@@ -19,4 +44,17 @@ showTimes = () => {
     result += i + ' ';
   }
   return result;
+}
+
+returnPage = () => {
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://news.ycombinator.com', {
+      waitUntil: 'networkidle2',
+    });
+    await page.pdf({ path: 'hn.pdf', format: 'a4' });
+
+    await browser.close();
+  })();
 }
